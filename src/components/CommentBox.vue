857394ec6c0181518011cbcf952609a1ac862107
Peter Wang <happypeter1983@gmail.com>
2017-10-11 14:25:10 +0800

    input

--- src/components/CommentBox.vue
+++ src/components/CommentBox.vue
@@ -1,19 +1,31 @@
 <template>
-  <ul>
+  <div>
+    <input v-model="text" placeholder="comment">
+    <button @click="handleClick">提交评论</button>
+    <ul>
       <li v-for="comment in comments">
         {{ comment.text }}
       </li>
     </ul>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'comment-box',
     data: () => ({
+      text: '',
       comments: [
         { text: 'Foo' },
         { text: 'Bar' },
       ],
-    })
+    }),
+    methods: {
+      handleClick: function () {
+        console.log('xxxx', this.text)
+        this.comments.push({ text: this.text })
+        this.text = ''
+      }
+    }
   }
 </script>
